99f307a7d23e5066628a25a5339a022690c59b76
VzzPK <p4blo.kl@gmail.com>
2024-12-14 15:40:29 +0100

    Wykonano

---
+++ zadania/math/index.js
@@ -0,0 +1,12 @@
+import add from './add.js';
+import substract from './subtract.js';
+import multiple from './multiply.js';
+import divide from './divide.js';
+
+const MATEMATYKA = {
+    add,
+    substract,
+    multiple,
+    divide
+  };
+  export default MATEMATYKA;
